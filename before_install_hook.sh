bb47f14fb6a423bc1cd4fcb3186edc1056342a31
[bogdanro] <gitlab@bogdanr.ro>
2017-10-16 11:41:14 +0300

    fix yarn install

--- before_install_hook.sh
+++ before_install_hook.sh
@@ -10,7 +10,14 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 # install nodejs
 yum -y install nodejs
 
+
+
+
+
 # install yarn
+
+wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo
+
 yum -y install yarn
 
 
