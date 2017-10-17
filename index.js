23e825b8a723158c2571f3f3284ce222633bb65b
[bogdanro] <gitlab@bogdanr.ro>
2017-10-17 16:38:33 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World, from github!')
+  response.send('Hello World, from github,utomatic push')
 })
 
 app.listen(app.get('port'), function() {
