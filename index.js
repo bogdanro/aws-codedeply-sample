64eac2ef7547eaaf2a7767922b0367353b0b4358
[bogdanro] <gitlab@bogdanr.ro>
2017-10-18 11:17:44 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World, from github,utomatic push')
+  response.send('Hello World, from github,utomatic push codepipeline beanstalk')
 })
 
 app.listen(app.get('port'), function() {
